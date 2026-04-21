a7e4ed2f528feb3629107b5abc538af95002073c
Leonardo Chavez <leonardo.chavroj@gmail.com>
2026-04-20 19:42:49 -0500

    Versión 2, agregando correción calibración de temperatura

--- lab_sem5.py
+++ lab_sem5.py
@@ -1,27 +1,47 @@
-# Módulo de monitoreo V1
+# Módulo de monitoreo V2
 
 # 1. Alcance Global: Variable accesible por cualquier función
 LIMITE_ALERTA_GLOBAL = 85.0
 
-# 2. Función sin retorno (Procedimiento)
+# ========================================================================
+
+# 2. Definir funciones
+def calibrar_sensor(temperatura, ajuste):
+    temperatura_calibrada = temperatura + ajuste
+    return temperatura_calibrada
+
 def mostrar_encabezado():
     print("=========================================")
     print("     SISTEMA DE MONITOREO INDUSTRIAL     ")
     print("=========================================")
 
-# 3. Función con retorno
+# ========================================================================
+
+# 3. Muestra del encabezado
+
+mostrar_encabezado()
+lectura_sensor = float(input("ingrese la temperatura del motor (°C): "))
+
+print("\n--- Modo Calibración ---")
+ajuste_tecnico = 2.5
+lectura_sensor = calibrar_sensor(lectura_sensor, ajuste_tecnico)
+print(f"Temperatura tras calibración de {ajuste_tecnico}°C: {lectura_sensor}°C")
+
+# ========================================================================
+
+# 4. Función con retorno
 def validar_temperatura(temp_actual):
     if temp_actual > LIMITE_ALERTA_GLOBAL:
         return True     # Hay alerta
     
     return False        #Todo normal
 
-# Ejecución principal
-mostrar_encabezado()
-lectura_sensor = float(input("ingrese la temperatura del motor (°C): "))
+# ========================================================================
+
+# 5. Ejecución
 
 if validar_temperatura(lectura_sensor):
-    print ("¡PELIGRO! Temperatura excede el lpimite operativo.")
+    print ("¡PELIGRO! Temperatura excede el límite operativo.")
 
 else:
     print ("Estado del motor: Operativo y estable.")
